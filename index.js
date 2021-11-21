d0a2a3a6388a5fb92e185c4512173b97abc25ef5
Sammy Taylor <sam.taylor@me.com>
2021-11-20 19:04:53 -0600

    Run dumb game in index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const SolitaireGame = require("./src/SolitaireGame")
 
 const game = new SolitaireGame()
 game.start()
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+async function dumbGame() {
+  while (true) {
+    await wait(1000)
+    game.displayGame()
+    const move = game.availableMoves()[0]
+    console.log("move", move)
+    if (!move) return
+    game.performMove(0)
+  }
+}
+
+dumbGame()
